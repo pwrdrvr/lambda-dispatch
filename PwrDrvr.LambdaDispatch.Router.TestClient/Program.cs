3e872042807ebdab6a59402ec0ee72f88856aeb5
harold <harold@pwrdrvr.com>
2023-11-30 14:32:51 -0800

    Uses Pipe but still reads entire request stream before sending

--- PwrDrvr.LambdaDispatch.Router.TestClient/Program.cs
+++ PwrDrvr.LambdaDispatch.Router.TestClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.IO.Pipelines;
 
 namespace PwrDrvr.LambdaDispatch.Router.TestClient;
 
@@ -6,44 +7,41 @@ public class Program
 {
   static public async Task TestChunkedRequest()
   {
-    using var client = new HttpClient();
-    client.BaseAddress = new Uri("http://localhost:5001");
+    using (var client = new HttpClient())
+    {
+      client.BaseAddress = new Uri("http://localhost:5001");
 
-    // Prepare the request
-    var request = new HttpRequestMessage(HttpMethod.Post, "/api/chunked");
+      // Prepare the request
+      var request = new HttpRequestMessage(HttpMethod.Post, "/api/chunked");
 
-    // Set transfer encoding to chunked
-    request.Headers.TransferEncodingChunked = true;
+      // Create a pipe to send the request body in chunks
+      var pipe = new Pipe();
+      request.Content = new StreamContent(pipe.Reader.AsStream());
 
+      // Start sending the request
+      var responseTask = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
 
-    // Set the request body to a PushStreamContent that sends a chunked request body
-    request.Content = new StreamRequestContent("Chunked request");
+      // Write the request body in chunks
+      await using (var writer = new StreamWriter(pipe.Writer.AsStream()))
+      {
+        for (int i = 0; i < 10; i++)
+        {
+          await writer.WriteAsync($"Chunk {i}\n");
+          await writer.FlushAsync();
 
-    // Send the request
-    var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+          // Sleep for 1 second between chunks
+          await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+      }
 
+      // Wait for the response
+      var response = await responseTask;
 
-    // new StreamContent(async (stream, content, context) =>
-    // {
-    //   using (var writer = new StreamWriter(stream))
-    //   {
-    //     for (int i = 0; i < 10; i++)
-    //     {
-    //       await writer.WriteAsync($"Chunk {i}\n");
-    //       await writer.FlushAsync();
-
-    //       // Sleep for 1 second between chunks
-    //       await Task.Delay(TimeSpan.FromSeconds(1));
-    //     }
-    //   }
-    // });
-
-
-    // Print the response body to the console
-    var responseBody = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(responseBody);
+      // Print the response body to the console
+      var responseBody = await response.Content.ReadAsStringAsync();
+      Console.WriteLine(responseBody);
+    }
   }
-
   static async Task Main(string[] args)
   {
     await TestChunkedRequest();
