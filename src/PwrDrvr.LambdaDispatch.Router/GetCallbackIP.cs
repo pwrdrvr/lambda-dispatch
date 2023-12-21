a19c36abcc40ed01ce75ec0114a9b09ef44b7b35
Harold Hunt <harold@pwrdrvr.com>
2023-12-21 09:14:46 -0500

    Fix local 5 second delay on 1st request (#32)
    - Was due to 5 second timeout on using EC2 endpoint to lookup IP address of the container

--- src/PwrDrvr.LambdaDispatch.Router/GetCallbackIP.cs
+++ src/PwrDrvr.LambdaDispatch.Router/GetCallbackIP.cs
@@ -8,45 +8,81 @@ public class GetCallbackIP
 {
   static private string? callbackUrl = null;
 
+  static private int performedInit = 0;
+
+  static private ReaderWriterLockSlim rwLock = new();
+
   static public async Task<string> Get()
   {
+    // Once it is set, it is set, we don't have to lock
     if (callbackUrl != null)
     {
       return callbackUrl;
     }
 
-    using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-    using var client = new HttpClient();
     try
     {
-      var response = await client.GetStringAsync("http://169.254.170.2/v2/metadata", cts.Token);
-      Console.WriteLine(response);
-      var metadata = JsonDocument.Parse(response).RootElement;
-      // On ECS there is an extra Containers parent that is not in EC2
-      var containers = metadata.GetProperty("Containers");
-      foreach (var container in containers.EnumerateArray())
+      // Wait for the one init to finish
+      rwLock.EnterUpgradeableReadLock();
+
+      // Only one thread should do the init
+      if (Interlocked.CompareExchange(ref performedInit, 1, 0) == 1)
       {
-        var networks = container.GetProperty("Networks");
-        foreach (var network in networks.EnumerateArray())
+        if (callbackUrl == null)
         {
-          if (network.GetProperty("NetworkMode").GetString() == "awsvpc")
+          throw new InvalidOperationException("Callback URL is null");
+        }
+        return callbackUrl;
+      }
+
+      // Upgrade the read lock to a write lock
+      rwLock.EnterWriteLock();
+
+      // We are the only thread that will get here, ever
+      using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+      using var client = new HttpClient();
+      try
+      {
+        var response = await client.GetStringAsync("http://169.254.170.2/v2/metadata", cts.Token);
+        Console.WriteLine(response);
+        var metadata = JsonDocument.Parse(response).RootElement;
+        // On ECS there is an extra Containers parent that is not in EC2
+        var containers = metadata.GetProperty("Containers");
+        foreach (var container in containers.EnumerateArray())
+        {
+          var networks = container.GetProperty("Networks");
+          foreach (var network in networks.EnumerateArray())
           {
-            callbackUrl = $"http://{network.GetProperty("IPv4Addresses").EnumerateArray().First().GetString()}:5001/api/chunked";
-            return callbackUrl;
+            if (network.GetProperty("NetworkMode").GetString() == "awsvpc")
+            {
+              callbackUrl = $"http://{network.GetProperty("IPv4Addresses").EnumerateArray().First().GetString()}:5001/api/chunked";
+              return callbackUrl;
+            }
           }
         }
       }
-    }
-    catch (HttpRequestException)
-    {
-      // Ignore
-    }
-    catch (OperationCanceledException)
-    {
-      // Ignore
-    }
+      catch (HttpRequestException)
+      {
+        // Ignore
+      }
+      catch (OperationCanceledException)
+      {
+        // Ignore
+      }
 
-    callbackUrl = $"http://{System.Environment.GetEnvironmentVariable("ROUTER_CALLBACK_HOST") ?? "127.0.0.1"}:5003/api/chunked";
-    return callbackUrl;
+      callbackUrl = $"http://{System.Environment.GetEnvironmentVariable("ROUTER_CALLBACK_HOST") ?? "127.0.0.1"}:5003/api/chunked";
+      return callbackUrl;
+    }
+    finally
+    {
+      if (rwLock.IsWriteLockHeld)
+      {
+        rwLock.ExitWriteLock();
+      }
+      if (rwLock.IsUpgradeableReadLockHeld)
+      {
+        rwLock.ExitUpgradeableReadLock();
+      }
+    }
   }
 }
