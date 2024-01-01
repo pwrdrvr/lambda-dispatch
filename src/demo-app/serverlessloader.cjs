82e025f5614e43b3f87d46fdea42db9766a2b64e
Harold Hunt <harold@pwrdrvr.com>
2023-12-31 21:47:57 -0500

    Issue 52 - Fix 502 from ALB on Direct Lambda (#53)

--- src/demo-app/serverlessloader.cjs
+++ src/demo-app/serverlessloader.cjs
@@ -1,7 +1,25 @@
 //
 // This is the entry point when deploying the app as a direct-invoked Lambda fronted by an ALB, API Gateway, or Function URL
 //
-const serverlessExpress = require("@codegenie/serverless-express");
+const { ServerlessAdapter } = require("@h4ad/serverless-adapter");
+const {
+  ExpressFramework,
+} = require("@h4ad/serverless-adapter/lib/frameworks/express");
+const {
+  DefaultHandler,
+} = require("@h4ad/serverless-adapter/lib/handlers/default");
+const {
+  PromiseResolver,
+} = require("@h4ad/serverless-adapter/lib/resolvers/promise");
+const { AlbAdapter } = require("@h4ad/serverless-adapter/lib/adapters/aws");
 const { app, performInit } = require("./app.cjs");
-exports.handler = serverlessExpress({ app });
+
+exports.handler = ServerlessAdapter.new(app)
+  .setFramework(new ExpressFramework())
+  .setHandler(new DefaultHandler())
+  .setResolver(new PromiseResolver())
+  .setRespondWithErrors(true)
+  .addAdapter(new AlbAdapter())
+  .build();
+
 exports.performInit = performInit;
