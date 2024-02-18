430a8960cf671a5430182bb53b6a330eb49189e3
Harold Hunt <harold@pwrdrvr.com>
2024-02-18 14:34:16 -0500

    Issue-124 - Simple desired instance count calc

---
+++ test/PwrDrvr.LambdaDispatch.Router.Tests/CapacityManagerTests.cs
@@ -0,0 +1,20 @@
+namespace PwrDrvr.LambdaDispatch.Router.Tests;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class CapacityManagerTests
+{
+  [TestCase(0, 0, 0, 0, ExpectedResult = 0)]
+  [TestCase(10, 5, 2, 2, ExpectedResult = 1)]
+  [TestCase(20, 10, 5, 2, ExpectedResult = 1)]
+  [TestCase(10, 2, 100, 2, ExpectedResult = 5)]
+  // 47 would give 235 target connections but 940 total connections
+  [TestCase(20, 4, 1000, 110, ExpectedResult = 47)]
+  public int SimpleDesiredInstanceCount_ReturnsExpectedResult(
+    int maxConcurrentCount, int instanceCountMultiplier, int pendingRequests, int runningRequests)
+  {
+    var capacityManager = new CapacityManager(maxConcurrentCount, instanceCountMultiplier);
+    return capacityManager.SimpleDesiredInstanceCount(pendingRequests, runningRequests);
+  }
+}
--- test/PwrDrvr.LambdaDispatch.Router.Tests/WeightedAverageTests.cs
+++ test/PwrDrvr.LambdaDispatch.Router.Tests/WeightedAverageTests.cs
@@ -1,3 +1,5 @@
+namespace PwrDrvr.LambdaDispatch.Router.Tests;
+
 using PwrDrvr.LambdaDispatch.Router;
 
 [TestFixture]
