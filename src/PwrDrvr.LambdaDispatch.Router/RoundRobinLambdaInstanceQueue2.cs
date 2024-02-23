e7b86e0129c1b1c3deeed1323e5d29f49e6e2cb5
Harold Hunt <harold@pwrdrvr.com>
2024-02-23 17:29:39 -0500

    RoundRobin2 - Version that does not remove/readd

---
+++ src/PwrDrvr.LambdaDispatch.Router/RoundRobinLambdaInstanceQueue2.cs
@@ -0,0 +1,112 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace PwrDrvr.LambdaDispatch.Router;
+
+/// <summary>
+/// A round-robin queue for Lambda instances.
+/// Does not remove and re-add instances from a queue while getting a connection.
+/// This should reduce CPU and increase the chances that an incoming request can
+/// get a connection.
+/// </summary>
+public class RoundRobinLambdaInstanceQueue2() : ILambdaInstanceQueue
+{
+  private List<ILambdaInstance> _list = [];
+
+  private readonly ReaderWriterLockSlim _rwlsQueue = new();
+
+  private volatile int _prevSlot = -1;
+
+  public void AddInstance(ILambdaInstance instance)
+  {
+    try
+    {
+      _rwlsQueue.EnterWriteLock();
+      _list.Add(instance);
+    }
+    finally
+    {
+      _rwlsQueue.ExitWriteLock();
+    }
+  }
+
+  public bool TryGetConnection([NotNullWhen(true)] out LambdaConnection? connection, bool tentative = false)
+  {
+    connection = null;
+
+    try
+    {
+      _rwlsQueue.EnterReadLock();
+
+      var approximateCount = 2 * _list.Count;
+
+      while (approximateCount-- >= 0)
+      {
+        var slot = Interlocked.Increment(ref _prevSlot);
+        if (_list.Count == 0)
+        {
+          return false;
+        }
+        if (slot >= _list.Count)
+        {
+          slot = 0;
+          Interlocked.Exchange(ref _prevSlot, 0);
+        }
+        var instance = _list[slot];
+
+        // We have to skip !Open instances because they will log errors
+        if (!instance.IsOpen)
+        {
+          // Skip this it will get cleaned up later
+          continue;
+        }
+
+        if (instance.TryGetConnection(out connection, tentative))
+        {
+          return true;
+        }
+      }
+
+      return false;
+    }
+    finally
+    {
+      _rwlsQueue.ExitReadLock();
+    }
+  }
+
+  public bool TryRemoveInstance([NotNullWhen(true)] out ILambdaInstance? instance)
+  {
+    try
+    {
+      _rwlsQueue.EnterWriteLock();
+
+      // Remove all !IsOpen instances
+      _list = _list.Where(i => i.IsOpen).ToList();
+
+      // Check if we have any instances left
+      if (_list.Count == 0)
+      {
+        instance = null;
+        return false;
+      }
+
+      // Give back the first instance, which should be the oldest
+      instance = _list[0];
+      _list.RemoveAt(0);
+      return true;
+    }
+    finally
+    {
+      if (_prevSlot >= _list.Count)
+      {
+        Interlocked.Exchange(ref _prevSlot, 0);
+      }
+      _rwlsQueue.ExitWriteLock();
+    }
+  }
+
+  public bool ReinstateFullInstance(ILambdaInstance instance)
+  {
+    return false;
+  }
+}
---
+++ test/PwrDrvr.LambdaDispatch.Router.Tests/RoundRobinLambdaInstanceQueue2Tests.cs
@@ -0,0 +1,177 @@
+using Moq;
+
+namespace PwrDrvr.LambdaDispatch.Router.Tests;
+
+[TestFixture]
+public class RoundRobinLambdaInstanceQueue2Tests
+{
+  private RoundRobinLambdaInstanceQueue2 _queue;
+  private Mock<ILambdaInstance> _mockInstance;
+
+  [SetUp]
+  public void SetUp()
+  {
+    _queue = new RoundRobinLambdaInstanceQueue2();
+    _mockInstance = new Mock<ILambdaInstance>();
+  }
+
+  [Test]
+  public void AddInstance_ShouldAddInstanceToQueue()
+  {
+    _mockInstance.SetupGet(i => i.IsOpen).Returns(true);
+    _queue.AddInstance(_mockInstance.Object);
+  }
+
+  [Test]
+  public void TryRemoveInstance_ShouldCleanQueue()
+  {
+    Mock<ILambdaInstance> _mockClosedInstance = new Mock<ILambdaInstance>();
+    Mock<ILambdaInstance> _mockOpenInstance = new Mock<ILambdaInstance>();
+
+    _mockClosedInstance.SetupGet(i => i.IsOpen).Returns(false);
+    _mockOpenInstance.SetupGet(i => i.IsOpen).Returns(true);
+    _mockOpenInstance.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>())).Returns(true);
+    _queue.AddInstance(_mockClosedInstance.Object);
+    _queue.AddInstance(_mockClosedInstance.Object);
+    _queue.AddInstance(_mockClosedInstance.Object);
+    _queue.AddInstance(_mockOpenInstance.Object);
+    _queue.AddInstance(_mockClosedInstance.Object);
+
+    Assert.That(_queue.TryGetConnection(out LambdaConnection? connection), Is.True);
+    Assert.That(_queue.TryGetConnection(out connection), Is.True);
+    _mockOpenInstance.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(2));
+    _mockOpenInstance.Verify(i => i.IsOpen, Times.Exactly(2));
+    _mockClosedInstance.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Never);
+    _mockClosedInstance.Verify(i => i.IsOpen, Times.Exactly(7));
+
+    Assert.That(_queue.TryRemoveInstance(out ILambdaInstance? instance), Is.True);
+    Assert.That(instance, Is.EqualTo(_mockOpenInstance.Object));
+    _mockOpenInstance.Verify(i => i.IsOpen, Times.Exactly(3));
+    _mockClosedInstance.Verify(i => i.IsOpen, Times.Exactly(11));
+
+    Assert.That(_queue.TryGetConnection(out connection), Is.False);
+    _mockOpenInstance.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(2));
+
+    // Nothing left to remove
+    Assert.That(_queue.TryRemoveInstance(out instance), Is.False);
+
+    // Open the closed instance
+    // Show that we still get no connection
+    _mockClosedInstance.SetupGet(i => i.IsOpen).Returns(true);
+    _queue.AddInstance(_mockInstance.Object);
+    Assert.That(_queue.TryGetConnection(out connection), Is.False);
+    _mockClosedInstance.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Never);
+    Assert.That(_queue.TryRemoveInstance(out instance), Is.False);
+  }
+
+  [Test]
+  public void TryGetConnection_ShouldReturnFalseIfNoInstances()
+  {
+    Assert.That(_queue.TryGetConnection(out LambdaConnection? connection), Is.False);
+  }
+
+  [Test]
+  public void TryGetConnection_ShouldReturnTrueIfInstanceHasConnection()
+  {
+    _mockInstance.Setup(i => i.IsOpen).Returns(true);
+    _mockInstance.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>())).Returns(true);
+    _queue.AddInstance(_mockInstance.Object);
+
+    Assert.That(_queue.TryGetConnection(out LambdaConnection? connection), Is.True);
+  }
+
+  [Test]
+  public void TryGetConnection_ShouldReturnFalseIfInstanceHasNoConnection()
+  {
+    _mockInstance.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>())).Returns(false);
+    _queue.AddInstance(_mockInstance.Object);
+
+    Assert.That(_queue.TryGetConnection(out LambdaConnection? connection), Is.False);
+  }
+
+  [Test]
+  public void TryGetConnection_ShouldCallTryGetConnectionOnEachInstanceAndReturnConnectionFromLastInstance()
+  {
+    Mock<ILambdaInstance> _mockInstanceWithoutConnection;
+    Mock<ILambdaInstance> _mockInstanceWithConnection;
+
+    _queue = new RoundRobinLambdaInstanceQueue2();
+    _mockInstanceWithoutConnection = new Mock<ILambdaInstance>();
+    _mockInstanceWithConnection = new Mock<ILambdaInstance>();
+
+    _mockInstanceWithoutConnection.Setup(i => i.IsOpen).Returns(true);
+    _mockInstanceWithoutConnection.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>())).Returns(false);
+    _mockInstanceWithConnection.Setup(i => i.IsOpen).Returns(true);
+    _mockInstanceWithConnection.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>())).Returns(true);
+
+    for (int i = 0; i < 9; i++)
+    {
+      _queue.AddInstance(_mockInstanceWithoutConnection.Object);
+    }
+
+    _queue.AddInstance(_mockInstanceWithConnection.Object);
+
+    Assert.That(_queue.TryGetConnection(out LambdaConnection? connection), Is.True);
+
+    _mockInstanceWithoutConnection.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(9));
+    _mockInstanceWithConnection.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Once);
+  }
+
+  [Test]
+  public void TryGetConnection_ShouldReturnConnectionFromCorrectInstance()
+  {
+    var mockInstance1 = new Mock<ILambdaInstance>();
+    var mockInstance2 = new Mock<ILambdaInstance>();
+    var mockInstance3 = new Mock<ILambdaInstance>();
+    var mockInstance4 = new Mock<ILambdaInstance>();
+
+    var callCount = 0;
+    mockInstance1.Setup(i => i.IsOpen).Returns(true);
+    mockInstance1.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()))
+        .Returns(() => callCount++ == 0 ? false : true);
+    mockInstance2.Setup(i => i.IsOpen).Returns(true);
+    mockInstance2.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>())).Returns(false);
+    mockInstance3.Setup(i => i.IsOpen).Returns(true);
+    mockInstance3.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>())).Returns(true);
+    mockInstance4.Setup(i => i.IsOpen).Returns(false);
+    mockInstance4.Setup(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>())).Returns(true);
+
+    _queue.AddInstance(mockInstance1.Object);
+    _queue.AddInstance(mockInstance2.Object);
+    _queue.AddInstance(mockInstance3.Object);
+    _queue.AddInstance(mockInstance4.Object);
+
+    Assert.That(_queue.TryGetConnection(out LambdaConnection? connection), Is.True);
+    mockInstance1.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Once);
+    mockInstance2.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Once);
+    // This gives the connection on 1st call
+    mockInstance3.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Once);
+
+    Assert.That(_queue.TryGetConnection(out connection), Is.True);
+    // This gives the connection on the 2nd call
+    mockInstance1.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(2));
+    mockInstance2.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Once);
+    mockInstance3.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Once);
+
+    Assert.That(_queue.TryGetConnection(out connection), Is.True);
+    mockInstance1.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(2));
+    mockInstance2.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(2));
+    // This gives the connection on the 3rd call
+    mockInstance3.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(2));
+
+    Assert.That(_queue.TryGetConnection(out connection), Is.True);
+    // This gives the connection on the 4th call
+    mockInstance1.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(3));
+    mockInstance2.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(2));
+    mockInstance3.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(2));
+
+    Assert.That(_queue.TryGetConnection(out connection), Is.True);
+    mockInstance1.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(3));
+    mockInstance2.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(3));
+    // This gives the connection on the 5th call
+    mockInstance3.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Exactly(3));
+
+    mockInstance4.Verify(i => i.IsOpen, Times.Exactly(2));
+    mockInstance4.Verify(i => i.TryGetConnection(out It.Ref<LambdaConnection?>.IsAny, It.IsAny<bool>()), Times.Never);
+  }
+}
