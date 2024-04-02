72a2a448662cc0e9f765575c2ddc38adb3661ed3
Harold Hunt <harold@pwrdrvr.com>
2024-04-01 21:31:06 -0400

    Continue using the app connection for health checks once established

--- extension/src/app_start.rs
+++ extension/src/app_start.rs
@@ -2,9 +2,74 @@ use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
 use std::time::Duration;
 
-use hyper::{body::Bytes, Request, Uri};
+use http_body_util::Empty;
+use hyper::{
+  body::{Bytes, Incoming},
+  Request, Uri,
+};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpStream;
+
+async fn create_connection(
+  healthcheck_addr: &str,
+) -> Option<(
+  hyper::client::conn::http1::SendRequest<Empty<Bytes>>,
+  hyper::client::conn::http1::Connection<TokioIo<TcpStream>, Empty<Bytes>>,
+)> {
+  let tcp_stream = match TcpStream::connect(healthcheck_addr).await {
+    Err(err) => {
+      log::debug!(
+        "Health check - Failed to connect to contained app: {:?}",
+        err
+      );
+      return None;
+    }
+    Ok(tcp_stream) => {
+      log::debug!("Health check - Connected to contained app");
+      tcp_stream
+    }
+  };
+  tcp_stream.set_nodelay(true).unwrap();
+  let io = TokioIo::new(tcp_stream);
+  match hyper::client::conn::http1::handshake(io).await {
+    Err(err) => {
+      log::debug!(
+        "Health check - Failed to handshake with contained app: {:?}",
+        err
+      );
+      None
+    }
+    Ok((sender, conn)) => {
+      log::info!("Health check - Handshake with contained app success");
+      Some((sender, conn))
+    }
+  }
+}
+
+async fn send_request(
+  sender: &mut hyper::client::conn::http1::SendRequest<Empty<Bytes>>,
+) -> Option<hyper::Response<Incoming>> {
+  let req = Request::builder()
+    .method("GET")
+    .uri("/health")
+    .header(hyper::header::HOST, "localhost")
+    .body(http_body_util::Empty::<Bytes>::new())
+    .unwrap();
+
+  match sender.send_request(req).await {
+    Err(err) => {
+      log::debug!(
+        "Health check - Failed to send request to contained app: {:?}",
+        err
+      );
+      None
+    }
+    Ok(res) => {
+      log::debug!("Health check - Send request to contained app success");
+      Some(res)
+    }
+  }
+}
 
 pub async fn health_check_contained_app(
   goaway_received: Arc<AtomicBool>,
@@ -15,81 +80,61 @@ pub async fn health_check_contained_app(
   let healthcheck_addr = format!("{}:{}", healthcheck_host, healthcheck_port);
 
   log::info!(
-    "Health check contained app at: {}",
+    "Health check - Starting for contained app at: {}",
     healthcheck_url.to_string()
   );
 
+  let mut sender = None;
+  let mut conn = None;
+
   while goaway_received.load(std::sync::atomic::Ordering::Acquire) == false {
-    // Delay 10 ms
     tokio::time::sleep(Duration::from_millis(10)).await;
 
-    // Create http connection
-    let tcp_stream = match TcpStream::connect(healthcheck_addr.clone()).await {
-      Err(err) => {
-        log::debug!("Failed to connect to contained app: {:?}", err);
-        continue;
+    if sender.is_none() || conn.is_none() {
+      let connection = create_connection(&healthcheck_addr).await;
+      if let Some((s, c)) = connection {
+        sender = Some(s);
+        conn = Some(tokio::task::spawn(async move {
+          if let Err(err) = c.await {
+            log::error!("Health check - Connection failed: {:?}", err);
+          }
+        }));
       }
-      Ok(tcp_stream) => {
-        log::debug!("Connected to contained app");
-        tcp_stream
-      }
-    };
-    tcp_stream.set_nodelay(true).unwrap();
-    let io = TokioIo::new(tcp_stream);
-    let (mut sender, conn) = match hyper::client::conn::http1::handshake(io).await {
-      Err(err) => {
-        log::debug!("Failed to handshake with contained app: {:?}", err);
-        continue;
-      }
-      Ok((sender, conn)) => {
-        log::debug!("Handshake with contained app success");
-        (sender, conn)
-      }
-    };
-    tokio::task::spawn(async move {
-      if let Err(err) = conn.await {
-        log::error!("Healthcheck connection failed: {:?}", err);
-      }
-    });
-    let req = Request::builder()
-      .method("GET")
-      .uri("/health")
-      .header(hyper::header::HOST, "localhost")
-      .body(http_body_util::Empty::<Bytes>::new())
-      .unwrap();
-
-    while futures::future::poll_fn(|ctx| sender.poll_ready(ctx))
-      .await
-      .is_err()
-    {
-      // This gets hit when the connection faults
       continue;
     }
 
-    let res = match sender.send_request(req).await {
-      Err(err) => {
-        log::debug!("Failed to send request to contained app: {:?}", err);
-        continue;
-      }
-      Ok(res) => {
-        log::debug!("Send request to contained app success");
-        res
-      }
-    };
-    let (parts, _) = res.into_parts();
-    if parts.status == hyper::StatusCode::OK {
-      log::info!("Health check complete - success");
+    let usable_sender = sender.as_mut().unwrap();
 
-      return true;
-    } else {
-      log::debug!("Health check failed: {:?}\nHeaders:", parts.status);
-      // Print all the headers received and the body
-      for header in parts.headers.iter() {
-        log::debug!("  {}: {}", header.0, header.1.to_str().unwrap());
+    if futures::future::poll_fn(|ctx| usable_sender.poll_ready(ctx))
+      .await
+      .is_err()
+    {
+      // The connection has errored
+      sender.take();
+      conn.take();
+      continue;
+    }
+
+    let res = send_request(usable_sender).await;
+
+    if let Some(res) = res {
+      let (parts, _) = res.into_parts();
+      if parts.status == hyper::StatusCode::OK {
+        log::info!("Health check - Complete - Success");
+        return true;
+      } else {
+        log::debug!("Health check - Failed: {:?}\nHeaders:", parts.status);
+        for header in parts.headers.iter() {
+          log::debug!("  {}: {}", header.0, header.1.to_str().unwrap());
+        }
       }
+    } else {
+      // The connection errored with a non-HTTP error
+      sender.take();
+      conn.take();
     }
   }
 
-  log::info!("Health check complete - failed");
-  return false;
+  log::info!("Health check - Complete - Failed");
+  false
 }
