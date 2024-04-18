9bce552593b54bb5e60fd00a9ac6e80ecf3db50f
Harold Hunt <harold@pwrdrvr.com>
2024-04-17 21:59:59 -0400

    Issue 155 - Add close tests

--- test/PwrDrvr.LambdaDispatch.Router.Tests/Areas/ControlChannels/Controllers/ChunkedControllerTests.cs
+++ test/PwrDrvr.LambdaDispatch.Router.Tests/Areas/ControlChannels/Controllers/ChunkedControllerTests.cs
@@ -2,6 +2,7 @@ using Moq;
 using NUnit.Framework;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using PwrDrvr.LambdaDispatch.Router.ControlChannels.Controllers;
 using PwrDrvr.LambdaDispatch.Router.EmbeddedMetrics;
 
@@ -57,5 +58,70 @@ public class ChunkedControllerTests
 
     // Assert
     Assert.That(result, Is.InstanceOf<NotFoundResult>());
+  }
+
+  [Test]
+  public async Task CloseInstance_WhenPoolExists_ClosesInstanceAndReturnsOk()
+  {
+    // Arrange
+    var poolId = "testPool";
+    var lambdaId = "testLambda";
+    var mockPool = new Mock<IPool>();
+    var mockDispatcher = new Mock<IDispatcher>();
+    IPool outPool = mockPool.Object;
+    mockPoolManager.Setup(p => p.GetPoolByPoolId(poolId, out outPool)).Returns(true);
+    mockPool.Setup(x => x.Dispatcher).Returns(mockDispatcher.Object);
+    mockPool.Setup(x => x.Dispatcher.CloseInstance(lambdaId, true)).Returns(Task.CompletedTask);
+
+    // Act
+    var result = await controller.CloseInstance(poolId, lambdaId);
+
+    // Assert
+    Assert.IsInstanceOf<OkResult>(result);
+    mockPool.Verify(x => x.Dispatcher.CloseInstance(lambdaId, true), Times.Once);
+  }
+
+  [Test]
+  public async Task CloseInstance_WhenPoolDoesNotExist_ReturnsNotFound()
+  {
+    // Arrange
+    var poolId = "testPool";
+    var lambdaId = "testLambda";
+    mockPoolManager.Setup(x => x.GetPoolByPoolId(poolId, out It.Ref<IPool>.IsAny)).Returns(false);
+
+    // Act
+    var result = await controller.CloseInstance(poolId, lambdaId);
+
+    // Assert
+    Assert.IsInstanceOf<NotFoundResult>(result);
+  }
+
+  [Test]
+  public async Task CloseInstance_WithoutPoolId_UsesGetPoolIdAndClosesInstance()
+  {
+    // Arrange
+    var poolId = "testPool";
+    var lambdaId = "testLambda";
+    var mockPool = new Mock<IPool>();
+    var mockDispatcher = new Mock<IDispatcher>();
+    mockDispatcher.Setup(x => x.CloseInstance(lambdaId, true)).Returns(Task.CompletedTask);
+    IPool outPool = mockPool.Object;
+    mockPoolManager.Setup(p => p.GetPoolByPoolId(poolId, out outPool)).Returns(true);
+    mockPool.Setup(x => x.Dispatcher).Returns(mockDispatcher.Object);
+
+    var httpContext = new DefaultHttpContext();
+    httpContext.Request.Headers["X-Pool-Id"] = poolId;
+
+    controller.ControllerContext = new ControllerContext
+    {
+      HttpContext = httpContext
+    };
+
+    // Act
+    var result = await controller.CloseInstance(lambdaId);
+
+    // Assert
+    Assert.IsInstanceOf<OkResult>(result);
+    mockPool.Verify(x => x.Dispatcher.CloseInstance(lambdaId, true), Times.Once);
   }
 }
