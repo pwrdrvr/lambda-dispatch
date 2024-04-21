8dd21447e24f6778524073ff285597ed79b8fcf2
Harold Hunt <harold@pwrdrvr.com>
2024-04-21 16:53:04 -0400

    Issue-159 - counter_drop tests

--- extension/src/counter_drop.rs
+++ extension/src/counter_drop.rs
@@ -7,3 +7,23 @@ impl<'a> Drop for DecrementOnDrop<'a> {
     self.0.fetch_sub(1, Ordering::SeqCst);
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::sync::Arc;
+  use tokio::time::{sleep, Duration};
+
+  #[tokio::test]
+  async fn test_decrement_on_drop() {
+    let counter = Arc::new(AtomicUsize::new(1));
+
+    {
+      let _decrement_on_drop = DecrementOnDrop(&counter);
+    } // _decrement_on_drop goes out of scope here and should be dropped
+
+    sleep(Duration::from_millis(100)).await; // give some time for the drop handler to run
+
+    assert_eq!(counter.load(Ordering::SeqCst), 0);
+  }
+}
