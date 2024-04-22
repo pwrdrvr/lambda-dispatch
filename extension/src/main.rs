dc511c8dc64dc763a9ecb1a296e8459e9073a084
Harold Hunt <harold@pwrdrvr.com>
2024-04-21 23:39:19 -0400

    Issue-159 - Main tests

--- extension/src/main.rs
+++ extension/src/main.rs
@@ -52,6 +52,10 @@ fn main() -> Result<()> {
     .without_time()
     .init();
 
+  startup()
+}
+
+fn startup() -> Result<()> {
   let options = Options::default();
 
   // If LAMBDA_DISPATCH_RUNTIME=default_multi_thread, use a multi-threaded runtime, let tokio decide how many threads
@@ -206,4 +210,200 @@ async fn my_extension(
     }
   }
   Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use httpmock::{Method::GET, MockServer};
+  use tokio::runtime::Runtime;
+  use tokio_test::assert_ok;
+
+  #[test]
+  fn test_startup() {
+    // Start app server
+    let app_server = MockServer::start();
+    let app_healthcheck_mock = app_server.mock(|when, then| {
+      when.method(GET).path("/health");
+      then.status(200).body("OK");
+    });
+
+    // let app_healthcheck_url: Uri = format!("{}/health", app_server.base_url()).parse().unwrap();
+
+    // Set the environment variable
+    std::env::set_var("AWS_LAMBDA_RUNTIME_API", "localhost:9001");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_NAME", "test_function");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_MEMORY_SIZE", "128");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_VERSION", "$LATEST");
+    std::env::set_var(
+      "LAMBDA_DISPATCH_PORT",
+      app_server.address().port().to_string(),
+    );
+
+    // Run the async_main function
+    let result = startup();
+
+    // Assert that the function returned successfully
+    assert_ok!(result);
+    // Assert app server's healthcheck endpoint got called
+    app_healthcheck_mock.assert();
+  }
+
+  #[test]
+  fn test_startup_default_multi_thread() {
+    // Start app server
+    let app_server = MockServer::start();
+    let app_healthcheck_mock = app_server.mock(|when, then| {
+      when.method(GET).path("/health");
+      then.status(200).body("OK");
+    });
+
+    // let app_healthcheck_url: Uri = format!("{}/health", app_server.base_url()).parse().unwrap();
+
+    // Set the environment variable
+    std::env::set_var("AWS_LAMBDA_RUNTIME_API", "localhost:9001");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_NAME", "test_function");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_MEMORY_SIZE", "128");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_VERSION", "$LATEST");
+    std::env::set_var(
+      "LAMBDA_DISPATCH_PORT",
+      app_server.address().port().to_string(),
+    );
+    std::env::set_var("LAMBDA_DISPATCH_RUNTIME", "default_multi_thread");
+
+    // Run the async_main function
+    let result = startup();
+
+    // Assert that the function returned successfully
+    assert_ok!(result);
+    // Assert app server's healthcheck endpoint got called
+    app_healthcheck_mock.assert();
+  }
+
+  #[test]
+  fn test_startup_multi_thread() {
+    // Start app server
+    let app_server = MockServer::start();
+    let app_healthcheck_mock = app_server.mock(|when, then| {
+      when.method(GET).path("/health");
+      then.status(200).body("OK");
+    });
+
+    // let app_healthcheck_url: Uri = format!("{}/health", app_server.base_url()).parse().unwrap();
+
+    // Set the environment variable
+    std::env::set_var("AWS_LAMBDA_RUNTIME_API", "localhost:9001");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_NAME", "test_function");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_MEMORY_SIZE", "128");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_VERSION", "$LATEST");
+    std::env::set_var(
+      "LAMBDA_DISPATCH_PORT",
+      app_server.address().port().to_string(),
+    );
+    std::env::set_var("LAMBDA_DISPATCH_RUNTIME", "multi_thread");
+
+    // Run the async_main function
+    let result = startup();
+
+    // Assert that the function returned successfully
+    assert_ok!(result);
+    // Assert app server's healthcheck endpoint got called
+    app_healthcheck_mock.assert();
+  }
+
+  #[test]
+  fn test_startup_multi_thread_tokio_worker_threads() {
+    // Start app server
+    let app_server = MockServer::start();
+    let app_healthcheck_mock = app_server.mock(|when, then| {
+      when.method(GET).path("/health");
+      then.status(200).body("OK");
+    });
+
+    // let app_healthcheck_url: Uri = format!("{}/health", app_server.base_url()).parse().unwrap();
+
+    // Set the environment variable
+    std::env::set_var("AWS_LAMBDA_RUNTIME_API", "localhost:9001");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_NAME", "test_function");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_MEMORY_SIZE", "128");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_VERSION", "$LATEST");
+    std::env::set_var(
+      "LAMBDA_DISPATCH_PORT",
+      app_server.address().port().to_string(),
+    );
+    std::env::set_var("LAMBDA_DISPATCH_RUNTIME", "multi_thread");
+    std::env::set_var("TOKIO_WORKER_THREADS", "4");
+
+    // Run the async_main function
+    let result = startup();
+
+    // Assert that the function returned successfully
+    assert_ok!(result);
+    // Assert app server's healthcheck endpoint got called
+    app_healthcheck_mock.assert();
+  }
+
+  #[test]
+  fn test_async_main() {
+    // Start app server
+    let app_server = MockServer::start();
+    let app_healthcheck_mock = app_server.mock(|when, then| {
+      when.method(GET).path("/health");
+      then.status(200).body("OK");
+    });
+
+    // let app_healthcheck_url: Uri = format!("{}/health", app_server.base_url()).parse().unwrap();
+
+    // Set the environment variable
+    std::env::set_var("AWS_LAMBDA_RUNTIME_API", "localhost:9001");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_NAME", "test_function");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_MEMORY_SIZE", "128");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_VERSION", "$LATEST");
+
+    let rt = Runtime::new().unwrap();
+    let mut options = Options::default();
+    options.async_init = false;
+    options.compression = false;
+    options.port = app_server.address().port();
+
+    // Run the async_main function
+    let result = rt.block_on(async_main(options));
+
+    // Assert that the function returned successfully
+    assert_ok!(result);
+    // Assert app server's healthcheck endpoint got called
+    app_healthcheck_mock.assert();
+  }
+
+  #[test]
+  fn test_async_main_async_init() {
+    // Start app server
+    let app_server = MockServer::start();
+    let app_healthcheck_mock = app_server.mock(|when, then| {
+      when.method(GET).path("/health");
+      then.status(200).body("OK");
+    });
+
+    // let app_healthcheck_url: Uri = format!("{}/health", app_server.base_url()).parse().unwrap();
+
+    // Set the environment variable
+    std::env::set_var("AWS_LAMBDA_RUNTIME_API", "localhost:9001");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_NAME", "test_function");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_MEMORY_SIZE", "128");
+    std::env::set_var("AWS_LAMBDA_FUNCTION_VERSION", "$LATEST");
+
+    let rt = Runtime::new().unwrap();
+    let mut options = Options::default();
+    options.async_init = true;
+    options.compression = false;
+    options.port = app_server.address().port();
+
+    // Run the async_main function
+    let result = rt.block_on(async_main(options));
+
+    // Assert that the function returned successfully
+    assert_ok!(result);
+    // Assert app server's healthcheck endpoint got called
+    app_healthcheck_mock.assert();
+  }
 }
